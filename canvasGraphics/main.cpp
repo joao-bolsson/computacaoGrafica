093973dcc8530eee858b38d2ffee59c5872569cc
João Bolsson <joaovictorbolsson@gmail.com>
2018-04-07 01:39:20 -0300

    inicia desenho da tela (botao)

--- canvasGraphics/main.cpp
+++ canvasGraphics/main.cpp
@@ -5,17 +5,47 @@
  * @since 2018, 01 Apr.
  */
 #include "src/gl_canvas2d.h"
-#include "src/File.h"
 
-short y_max = 100, y_min = -100;
+/**
+ * Initial values for width and hight of window.
+ */
+#define WIDTH 1000
+#define HEIGHT 700
+#define MARGIN_BUTTON_X 5
+#define MARGIN_BUTTON_Y 5
+// canvas use GLUT_BITMAP_8_BY_13 for draw text
+#define FONT_H 13
+#define FONT_W 8
+#define PIXEL_SCALE 3
+
+void drawButtons() {
+    const char *textLoad = "LOAD";
+    auto size = (short) strlen(textLoad);
+    int textWidth = size * FONT_W;
+
+    unsigned short x1 = 0, y1 = 0;
+    color(1, 0, 0);
+    rect(PIXEL_SCALE * (x1 + MARGIN_BUTTON_X), PIXEL_SCALE * (y1 + MARGIN_BUTTON_Y),
+         PIXEL_SCALE * (x1 + textWidth), PIXEL_SCALE * (y1 + FONT_H));
+    color(1, 1, 1);
+
+    text(PIXEL_SCALE * (x1 + MARGIN_BUTTON_X), PIXEL_SCALE * (y1 + MARGIN_BUTTON_Y), textLoad);
+}
+
+void drawWindow() {
+    clear(0, 0, 0); // tela com fundo preto
+
+    drawButtons();
+}
 
 void render() {
-    // P(0,0) está no canto inferior esquerdo
-    clear(1, 1, 1);
-    color(0, 0, 0);
-
-    // x rect
-    rect(0, altura / 2, largura, altura / 2);
+    drawWindow();
+//    // P(0,0) está no canto inferior esquerdo
+//    clear(1, 1, 1);
+//    color(0, 0, 0);
+//
+//    // x rect
+//    rect(0, altura / 2, largura, altura / 2);
 }
 
 //funcao chamada toda vez que uma tecla for pressionada
@@ -34,6 +64,7 @@ void mouse(int button, int state, int x, int y) {
 }
 
 int main() {
+    /*
     cout << "Caminho completo do Arquivo: ";
     string filePath;
     cin >> filePath;
@@ -49,8 +80,9 @@ int main() {
         cout << sig << endl;
     }
     cout << "=================" << endl;
+    */
 
-    initCanvas(600, 400, "Plano Cartesiano");
+    initCanvas(WIDTH, HEIGHT, "Plano Cartesiano");
     runCanvas();
 
     return 0;
