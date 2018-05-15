4419b48354eb8416dc5dbc9d104c2c6204d52d77
João Bolsson <joaovictorbolsson@gmail.com>
2018-05-14 23:05:51 -0300

    adiciona documentacao principal

--- gl_4_canvasQT/main.cpp
+++ gl_4_canvasQT/main.cpp
@@ -5,6 +5,32 @@
 **
 ****************************************************************************/
 
+/*
+Desenha linha, retangulo e curva. As primitivas são desenhadas apenas uma vez, ou seja,
+cada clique num botão vai desenhar (apos a interação do usuário) e vai parar de desenhar.
+Para desenhar duas vezes seguidas a mesma primitiva, deve-se clicar no botão, interagir e clicar de novo e interagir.
+
+Limpar: limpa o que está na tela
+Salvar: salva as primitivas desenhadas no arquivo
+Abrir: carrega as primitivas do arquivo na tela
+Excluir: remove a primitiva selecionada
+
+Retangulo e linha: para desenhar clica primeiro em um dos botões e depois clica na canvas e arrasta o mouse.
+Faz com que uma demonstração seja visualiza pelo usuário com a primitiva e a sua caixa seletora (na forma de pontos)
+
+rotação: é feita com o pivo sendo o primeiro ponto desenhado da primitiva
+
+rotação esquerda: rotaciona para a esquerda a primitiva selecionada
+rotação direita: rotaciona para a direita a primitiva selecionada
+
+Curva: o usuário clica no botão curva e insere 4 pontos na canvas, após isso a curva será desenhada e inserida
+
+Clicar sobre a area de uma primitiva faz com que ela seja selecionada.
+Clicar e arrastar uma primitiva selecionada faz com que ela seja movida de acordo com a posição do mouse.
+
+Clicar sobre um ponto de controle de uma curva e arrastar faz com que o ponto seja movido e a curva alterada.
+*/
+
 #include <QApplication>
 #include <QMainWindow>
 #include "mainwindow.h"
