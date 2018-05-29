04a2f4c4872901e144310d3bcd0158f323524435
Joao Bolsson <joaovictorbolsson@gmail.com>
2018-05-29 16:24:19 -0300

    muda ordem de desenho das faces

--- t3-jvmarques/cube.cpp
+++ t3-jvmarques/cube.cpp
@@ -18,19 +18,20 @@ Cube::Cube(int l, Point *c):Solid(c) {
 
 void Cube::draw(Canvas2D *canvas) {
     canvas->color(1, 0, 0);
-
-    // front face
-    canvas->line(points[0]->getX2d(), points[0]->getY2d(), points[1]->getX2d(), points[1]->getY2d());
-    canvas->line(points[1]->getX2d(), points[1]->getY2d(), points[2]->getX2d(), points[2]->getY2d());
-    canvas->line(points[2]->getX2d(), points[2]->getY2d(), points[3]->getX2d(), points[3]->getY2d());
-    canvas->line(points[3]->getX2d(), points[3]->getY2d(), points[0]->getX2d(), points[0]->getY2d());
-
     // back face
     canvas->line(points[4]->getX2d(), points[4]->getY2d(), points[5]->getX2d(), points[5]->getY2d());
     canvas->line(points[5]->getX2d(), points[5]->getY2d(), points[6]->getX2d(), points[6]->getY2d());
     canvas->line(points[6]->getX2d(), points[6]->getY2d(), points[7]->getX2d(), points[7]->getY2d());
     canvas->line(points[7]->getX2d(), points[7]->getY2d(), points[4]->getX2d(), points[4]->getY2d());
 
+    canvas->color(0, 1, 0);
+    // front face
+    canvas->line(points[0]->getX2d(), points[0]->getY2d(), points[1]->getX2d(), points[1]->getY2d());
+    canvas->line(points[1]->getX2d(), points[1]->getY2d(), points[2]->getX2d(), points[2]->getY2d());
+    canvas->line(points[2]->getX2d(), points[2]->getY2d(), points[3]->getX2d(), points[3]->getY2d());
+    canvas->line(points[3]->getX2d(), points[3]->getY2d(), points[0]->getX2d(), points[0]->getY2d());
+
+    canvas->color(0, 0, 1);
     // creating the cube
     canvas->line(points[0]->getX2d(), points[0]->getY2d(), points[4]->getX2d(), points[4]->getY2d());
     canvas->line(points[1]->getX2d(), points[1]->getY2d(), points[5]->getX2d(), points[5]->getY2d());
